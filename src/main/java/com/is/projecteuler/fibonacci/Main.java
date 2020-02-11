d059127d29226bfec6d95ce68a17907f8c8fe809
YarovoyYra <yarovoy1000@gmail.com>
2020-02-11 20:51:30 +0300

    count even-value of Fibonacci`s sequence

---
+++ src/main/java/com/is/projecteuler/fibonacci/Main.java
@@ -0,0 +1,12 @@
+package com.is.projecteuler.fibonacci;
+
+
+import static com.is.projecteuler.fibonacci.Finder.evenCounter;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(evenCounter());
+    }
+}
