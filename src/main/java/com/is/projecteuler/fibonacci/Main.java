739930522737408eacae54b388f2586f783c31cf
YarovoyYra <yarovoy1000@gmail.com>
2020-02-12 22:24:38 +0300

    refactor, method name, add javadoc, rebuilt isLoop

--- src/main/java/com/is/projecteuler/fibonacci/Main.java
+++ src/main/java/com/is/projecteuler/fibonacci/Main.java
@@ -1,12 +1,12 @@
 package com.is.projecteuler.fibonacci;
 
 
-import static com.is.projecteuler.fibonacci.Finder.evenCounter;
+import static com.is.projecteuler.fibonacci.Fibonacci.sumOfEvenValue;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(evenCounter());
+        System.out.println(sumOfEvenValue());
     }
 }
