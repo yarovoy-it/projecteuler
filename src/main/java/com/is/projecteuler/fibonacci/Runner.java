76abccac0fb38c290ecb871b0e26225b0716814f
yarovoy <yarovoy1000@gmail.com>
2020-02-13 18:03:05 +0300

    rewrited javadoc and name

---
+++ src/main/java/com/is/projecteuler/fibonacci/Runner.java
@@ -0,0 +1,12 @@
+package com.is.projecteuler.fibonacci;
+
+
+import static com.is.projecteuler.fibonacci.Fibonacci.sumOfFibonacciEvenValue;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+        System.out.println(sumOfFibonacciEvenValue(-122));
+    }
+}
