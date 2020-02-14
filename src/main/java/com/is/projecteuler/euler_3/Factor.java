88eb95d3da7f504ce25f4982f48fb200be99d943
yarovoy <yarovoy1000@gmail.com>
2020-02-14 17:20:40 +0300

    euler_3 add define simple divider and print

--- src/main/java/com/is/projecteuler/euler_3/Factor.java
+++ src/main/java/com/is/projecteuler/euler_3/Factor.java
@@ -1,9 +1,6 @@
 package com.is.projecteuler.euler_3;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * The prime factors of 13195 are 5, 7, 13 and 29.
@@ -13,44 +10,111 @@ import java.util.Map;
 public class Factor {
 
     /**
-     * From number method calculate the factors and return list
+     * From number method calculate the prime factors and return list
      *
-     * @param number which calculate the factors
-     * @return the list of factorials
+     * @param number which calculate the prime factors
+     * @return the list of prime factors
      */
-    public static List<Long> calculatorFactors(long number) {
+    public static Set<Long> definerPrimeFactors(long number) {
         if (number < 1) {
             throw new IllegalArgumentException("No reason looking for factor " + number);
         }
-        List<Long> factors = new ArrayList<>();
+        Set<Long> primeFactors = new HashSet<>();
         for (int divider = 2; divider <= number; divider++) {
             while (number % divider == 0) {
-                factors.add((long) divider);
+                primeFactors.add((long) divider);
                 number /= divider;
             }
         }
-        return factors;
+        return primeFactors;
     }
 
     /**
-     * Calculate factors from list then put in map, where key is number, value is list of factors
+     * Define factors from list then put in map, where key is number, value is list of prime factors
      *
      * @param numbers list of number
-     * @return map key is number value is list of factors
+     * @return map key is number value is list of prime factors
      */
-    public static Map<Long, List<Long>> calculatorFactors(List<Long> numbers) {
+    public static Map<Long, Set<Long>> definerPrimeFactors(List<Long> numbers) {
         if (numbers == null) {
-            throw new IllegalArgumentException("No reason looking for factors " + numbers);
+            throw new IllegalArgumentException("No reason looking for prime factors " + numbers);
         }
-        Map<Long, List<Long>> mapOfFactors = new HashMap<>();
-        for (Long numberForFactor : numbers) {
-            if (numberForFactor != null)
-                mapOfFactors.put(numberForFactor, calculatorFactors(numberForFactor));
+        Map<Long, Set<Long>> mapOfPrimeFactors = new HashMap<>();
+        for (Long numberForPrimeFactor : numbers) {
+            if (numberForPrimeFactor != null)
+                mapOfPrimeFactors.put(numberForPrimeFactor, definerPrimeFactors(numberForPrimeFactor));
         }
 
-        return mapOfFactors;
+        return mapOfPrimeFactors;
     }
 
+    /**
+     * Define all dividers
+     *
+     * @param number for defining list of divider
+     * @return list of dividers
+     */
+    public static List<Integer> definerAllDivider(long number) {
+        List<Integer> dividers = new ArrayList<>();
+        for (int divider = 1; divider <= number; divider++) {
+            if (number % divider == 0)
+                dividers.add(divider);
+        }
+        return dividers;
+    }
+
+    public static List<Integer> definerSimpleDivider(long number) {
+        Map<Integer, Integer> divinerDegree = new HashMap<>();
+        List<Integer> primeFactors = new ArrayList<>();
+        int countDegree = 0;
+        for (int divider = 2; divider <= number; divider++) {
+            while (number % divider == 0) {
+                primeFactors.add(divider);
+                number /= divider;
+                countDegree++;
+            }
+            countDegree = 0;
+        }
+        return primeFactors;
+    }
+
+    public static void printer(List<Integer> listNumbers) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int countRepeatValue = 0;
+        int foundNumber = 0;
+        for (int i = 0; i < listNumbers.size(); i++) {
+            int temp = listNumbers.get(i);
+            if (foundNumber != temp) {
+                foundNumber = listNumbers.get(i);
+                for (Integer listNumber : listNumbers) {
+                    if (foundNumber == listNumber) {
+                        countRepeatValue++;
+                    }
+                }
+                map.put(foundNumber, countRepeatValue);
+                countRepeatValue = 0;
+            }
+        }
+        int counter = 1;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            printSymbol(entry.getKey(), entry.getValue());
+            printSymbol(map.size() > counter);
+            counter++;
+        }
+    }
+
+    private static void printSymbol(Integer key, Integer value) {
+        if (value <= 1) {
+            System.out.print(key + " ");
+        } else {
+            System.out.print(key + "^" + value + "");
+        }
+    }
+
+    private static void printSymbol(boolean checker) {
+        if (checker == true)
+            System.out.print(" * ");
+    }
 }
 
 
--- src/main/java/com/is/projecteuler/euler_3/Runner.java
+++ src/main/java/com/is/projecteuler/euler_3/Runner.java
@@ -1,15 +1,24 @@
 package com.is.projecteuler.euler_3;
 
-import static com.is.projecteuler.euler_3.Factor.calculatorFactorial;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.is.projecteuler.euler_3.Factor.definerSimpleDivider;
 
 public class Runner {
 
     public static void main(String[] args) {
-//        List<Long> longs = new ArrayList<>();
-//        longs.add(13195L);
+        List<Long> longs = new ArrayList<>();
+//        System.out.println(definerSimpleDivider(2000));
+//        mapCollector(definerSimpleDivider(2000));
+//        printer();
+        System.out.println(definerSimpleDivider(20));
+//        definerSimpleDivider(20);
+//        longs.add(20L);
 //        longs.add(13195L);
 //        longs.add(600851475143L);
-        System.out.println(calculatorFactorial(13195));
+//        System.out.println(definerPrimeFactors(longs));
+
     }
 
 }
