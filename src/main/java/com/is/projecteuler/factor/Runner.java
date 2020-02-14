3d09c3ccc4ee10761dc538b6bb4dc90c8d39f7e6
yarovoy <yarovoy1000@gmail.com>
2020-02-14 11:39:55 +0300

    euler_3 overloud method entry parameter can be

--- src/main/java/com/is/projecteuler/factor/Runner.java
+++ src/main/java/com/is/projecteuler/factor/Runner.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static com.is.projecteuler.factor.Factorial.calculatorFactorial;
-import static com.is.projecteuler.factor.Factorial.calculatorFactorials;
 
 public class Runner {
 
@@ -12,10 +11,7 @@ public class Runner {
         List<Long> longs = new ArrayList<>();
         longs.add(13195L);
         longs.add(13195L);
-        longs.add(null);
         longs.add(600851475143L);
-
-        System.out.println(calculatorFactorials(longs));
         System.out.println(calculatorFactorial(13195));
     }
 
