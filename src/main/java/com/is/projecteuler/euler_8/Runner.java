7de486548aa8014809f5947c58bce09c15719509
yarovoy <yarovoy1000@gmail.com>
2020-02-17 17:59:13 +0300

    euler_8 count sum of 4 numbers need to update to 13 numbers

---
+++ src/main/java/com/is/projecteuler/euler_8/Runner.java
@@ -0,0 +1,11 @@
+package com.is.projecteuler.euler_8;
+
+import static com.is.projecteuler.euler_8.Searcher.stringToInt;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+        System.out.println(stringToInt(StringStorage.stringLoader()));
+    }
+}
