58b7a4abc1ae13c97549da4723b0e41fac75a916
yarovoy <yarovoy1000@gmail.com>
2020-02-18 11:56:30 +0300

    euler_9 few classes without any logic

---
+++ src/main/java/com/is/projecteuler/euler_9/PythagoreanTriplet.java
@@ -0,0 +1,13 @@
+package com.is.projecteuler.euler_9;
+
+/**
+ * A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+ * <p>
+ * a2 + b2 = c2
+ * For example, 32 + 42 = 9 + 16 = 25 = 52.
+ * <p>
+ * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+ * Find the product abc.
+ */
+public class PythagoreanTriplet {
+}
---
+++ src/main/java/com/is/projecteuler/euler_9/Runner.java
@@ -0,0 +1,9 @@
+package com.is.projecteuler.euler_9;
+
+public class Runner {
+
+    public static void main(String[] args){
+
+
+    }
+}
