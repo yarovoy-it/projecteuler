6e469d39993d9c5966562b8773c991bc66fe1429
YarovoyYra <yarovoy1000@gmail.com>
2020-02-23 22:07:58 +0300

    euler_12 didnt test no javadoc

---
+++ src/main/java/com/is/projecteuler/euler_12/Runner.java
@@ -0,0 +1,11 @@
+package com.is.projecteuler.euler_12;
+
+import static com.is.projecteuler.euler_12.DivisibleTriangular.findValueFirstTriangleNumber;
+import static com.is.projecteuler.euler_12.DivisibleTriangular.getCountNumberDividers;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+    }
+}
