7e0e625f6bcf52c85ae28b68803cc4d8da9f3b55
yarovoy <yarovoy1000@gmail.com>
2020-03-04 16:48:39 +0300

    euler_19 add counter method

--- src/main/java/com/is/projecteuler/euler_19/EulerCalendar.java
+++ src/main/java/com/is/projecteuler/euler_19/EulerCalendar.java
@@ -1,5 +1,18 @@
 package com.is.projecteuler.euler_19;
 
+/**
+ * You are given the following information, but you may prefer to do some research for yourself.
+ * <p>
+ * 1 Jan 1900 was a Monday.
+ * Thirty days has September,
+ * April, June and November.
+ * All the rest have thirty-one,
+ * Saving February alone,
+ * Which has twenty-eight, rain or shine.
+ * And on leap years, twenty-nine.
+ * A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+ * How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+ */
 public class EulerCalendar {
 
     private static final String STARTING_DATE = "1 Jan 1900 was a Monday";
@@ -14,6 +27,17 @@ public class EulerCalendar {
             february = 28;
         }
         return monthOf30Days + monthOf31Days + february;
+    }
+
+    public static int countSundaysByYear(int year) {
+        int countSundays = 0;
+        int restDays = 0;
+        for (int yearCount = 1901; yearCount < year; yearCount++) {
+            int days = getDaysOfYear(yearCount);
+            restDays = days % 7;
+            countSundays += (restDays + days) / 7;
+        }
+        return countSundays;
     }
 
 }
--- src/main/java/com/is/projecteuler/euler_19/Runner.java
+++ src/main/java/com/is/projecteuler/euler_19/Runner.java
@@ -1,10 +1,12 @@
 package com.is.projecteuler.euler_19;
 
-import static com.is.projecteuler.euler_19.EulerCalendar.getDaysOfYear;
+import static com.is.projecteuler.euler_19.EulerCalendar.countSundaysByYear;
 
 public class Runner {
 
-    public static void main(String[] args){
-        System.out.println(getDaysOfYear(2016));
+    public static void main(String[] args) {
+//        System.out.println(getDaysOfYear(2016));
+        System.out.println(countSundaysByYear(2000));
+
     }
 }
