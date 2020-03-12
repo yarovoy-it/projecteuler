e70606e2feef6f05e82de394e983f2c40dce17a2
yarovoy <yarovoy1000@gmail.com>
2020-03-12 15:24:44 +0300

    euler_21 rename branche

---
+++ src/main/java/com/is/projecteuler/euler_22/Runner.java
@@ -0,0 +1,12 @@
+package com.is.projecteuler.euler_22;
+
+import static com.is.projecteuler.euler_22.FileParser.getFile;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+        getFile();
+
+    }
+}
